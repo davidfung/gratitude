96acb95c708dc4a15a4b1efa914da9c066da850e
davidfung <davidfung@amgcomputing.com>
2021-06-09 22:29:49 -0700

    Provide default email address for export

--- lib/app/app_export.dart
+++ lib/app/app_export.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:gratitude/app/app_header.dart';
 import 'package:gratitude/provider/gratitude_provider.dart';
 import 'package:gratitude/util/email.dart';
+import 'package:gratitude/util/setting.dart';
 import 'package:provider/provider.dart';
 
 const SUBJECT = "My Gratitudes";
 
-void export(BuildContext context) {
+void export(BuildContext context) async {
   // Export the entire database by formatting it as text and send as email body.
   final content;
   Gratitudes gratitudes;
+  late final recipient;
+
+  recipient = await loadString(SETTING_EXPORT_EMAIL, defaultValue: "");
 
   gratitudes = Provider.of<Gratitudes>(context, listen: false);
   content = gratitudes.export();
 
   sendemail(
-    to: [],
+    to: [recipient],
     cc: [],
     bcc: [],
     subject: SUBJECT,
