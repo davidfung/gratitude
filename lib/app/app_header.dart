f5ff4756673e61c4e8ea4ac7e8ffe13a1bc92000
davidfung <davidfung@amgcomputing.com>
2021-07-23 01:02:24 -0700

    Add and set default icon to favorite

--- lib/app/app_header.dart
+++ lib/app/app_header.dart
@@ -33,24 +33,27 @@ MaterialColor secColor = MaterialColor(0xFFcddb89, greenColorMap);
 const String APP_TITLE = "Gratitude";
 
 // icon name should match the material icon counterpart for easy reference
-const int icon_default = 0;
+const int icon_default = 8;
 const int icon_add_alert = 1;
 const int icon_assistant_photo = 2;
 const int icon_attach_file = 3;
 const int icon_call = 4;
 const int icon_directions_bike = 5;
 const int icon_directions_walk = 6;
-const int icon_people = 7;
-const int icon_portrait = 8;
+const int icon_favorite = 7;
+const int icon_favorite_outline = 8;
+const int icon_people = 9;
+const int icon_portrait = 10;
 
 const Map<int, Icon> GIconSet = {
-  icon_default: Icon(Icons.add_alert),
   icon_add_alert: Icon(Icons.add_alert),
   icon_assistant_photo: Icon(Icons.assistant_photo),
   icon_attach_file: Icon(Icons.attach_file),
   icon_call: Icon(Icons.call),
   icon_directions_bike: Icon(Icons.directions_bike),
   icon_directions_walk: Icon(Icons.directions_walk),
+  icon_favorite: Icon(Icons.favorite),
+  icon_favorite_outline: Icon(Icons.favorite_outline),
   icon_people: Icon(Icons.people),
   icon_portrait: Icon(Icons.portrait),
 };
