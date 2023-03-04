f2956d0d34aea6a55c66288ba6909ed51bd94ba7
davidfung <davidfung@amgcomputing.com>
2023-03-03 21:23:43 -0800

    Replace package_info with package_info_plus

--- lib/app/app_about_view.dart
+++ lib/app/app_about_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 import 'app_header.dart';
 import '../util/web.dart';
